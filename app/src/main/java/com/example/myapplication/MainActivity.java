f9f20d8c60096796c18fc9a2cc184d50abda122d
Ali Emir Sen <ali.sen@softtech.com.tr>
2019-12-27 02:35:27 +0300

    done merely

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -18,21 +20,41 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.UUID;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private int REQUEST_CODE_PERMISSIONS = 101;
     private static final int VIDEO_CAPTURE = 102;
-    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
+
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.READ_EXTERNAL_STORAGE", "android.permission.INTERNET", "android.permission.ACCESS_NETWORK_STATE"};
+
     final ArrayList<TextView> tvArray = new ArrayList<TextView>(4);
     final ArrayList<String> predictionsArray = new ArrayList<String>(4);
+    TextView indicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         populatePredictionsArray();
+        indicator = findViewById(R.id.tvStatusIndicator);
+        indicator.setText("HAZIR");
         tvArray.add((TextView) findViewById(R.id.tv_latest_0));
         tvArray.add((TextView) findViewById(R.id.tv_latest_1));
         tvArray.add((TextView) findViewById(R.id.tv_latest_2));
@@ -50,6 +74,61 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void uploadFile(Uri fileUri) {
+
+        // create upload service client
+        FileUploadService service =
+                ServiceGenerator.createService(FileUploadService.class);
+
+        File file = new File(FileChooser.getPath(getBaseContext(),fileUri));
+        Log.v("File From URI", "success");
+        // create RequestBody instance from file
+        RequestBody requestFile =
+                RequestBody.create(
+                        MediaType.parse(getContentResolver().getType(fileUri)),
+                        file
+                );
+        Log.v("Request File Creation", "success");
+        // MultipartBody.Part is used to send also the actual file name
+        MultipartBody.Part body =
+                MultipartBody.Part.createFormData("file", file.getName(), requestFile);
+        Log.v("Multipart Body", "success");
+        // add another part within the multipart request
+        String descriptionString = "Video file to predict from";
+        RequestBody description =
+                RequestBody.create(
+                        okhttp3.MultipartBody.FORM, descriptionString);
+        Log.v("Request", "executing");
+        indicator.setText("GONDERILIYOR");
+        // finally, execute the request
+        Call<ResponseBody> call = service.upload(description, body);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call,
+                                   Response<ResponseBody> response) {
+                JsonObject a = null;
+                try {
+                    a = new JsonParser().parse(response.body().string()).getAsJsonObject();
+                    Toast.makeText(MainActivity.this, a.get("label").toString(), Toast.LENGTH_SHORT).show();
+                    setPrediction(a.get("label").toString().replace("\"", ""));
+                    Log.v("response as string", a.get("label").toString());
+                    Log.v("Upload", "success");
+                    indicator.setText("HAZIR (OK)");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    indicator.setText("HAZIR (ERR)");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e("Upload error:", t.getMessage());
+                setPrediction("Hata :(");
+                indicator.setText("HAZIR (ERR)");
+            }
+        });
+    }
+
     public void handleVideoButtonClick(View view) {
 
 
@@ -57,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
             //Snackbar.make(view, "Start Camera here", Snackbar.LENGTH_LONG)
             //        .setAction("Action", null).show();
             Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-            intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY,1);
+            intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY,0);
             intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 5);
             startActivityForResult(intent, VIDEO_CAPTURE);
 
@@ -75,10 +154,10 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 if (videoUri != null) {
                     String[] uriArr = videoUri.toString().split("/");
-                    setPrediction(uriArr[uriArr.length - 1].toString() + "pred");
+                    uploadFile(videoUri);
+                    indicator.setText("CEVAP BEKLENIYOR");
                 }
-                Toast.makeText(this, "Video bu yola kaydedildi:\n" +
-                        videoUri, Toast.LENGTH_LONG).show();
+
             } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "Kayıt iptal edildi.",
                         Toast.LENGTH_LONG).show();
@@ -140,7 +219,6 @@ public class MainActivity extends AppCompatActivity {
             tvArray.get(i).setText(predictionsArray.get(i));
         }
     }
-
 
     private boolean allPermissionsGranted(){
 
---
+++ app/src/main/java/com/example/myapplication/ServiceGenerator.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceGenerator {
+
+    private static final String BASE_URL = "http://104.248.136.192:80/";
+
+    private static Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit retrofit = builder.build();
+
+    private static OkHttpClient.Builder httpClient =
+            new OkHttpClient.Builder();
+
+
+    public static <S> S createService(
+            Class<S> serviceClass) {
+
+
+
+        return retrofit.create(serviceClass);
+    }
+}
+
